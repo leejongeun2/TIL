5618523c86699cde6b099f3f450675994053c55f
tmsnvl95 <tmsnvl95@naver.com>
2022-07-22 08:39:57 +0900

    0721update

--- python/0720_pratic.py
+++
@@ -1,3 +0,0 @@
-a = [3,2,1]
-sorted(a)
-print(a)
--- python/0720_swea_1933.py
+++
@@ -1,4 +0,0 @@
-a = int(input())
-for i in range(a): #range(a)를 이용해 a-1회 반복
-    if a%(i+1)==0: #i가 0부터 시작하기 때문에 i+1~n까지를 나눌 수 있게 해주고, 나누었을 때 나머지가 0이면 출력
-        print(i+1,end=" ")
