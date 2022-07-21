5eb950d11616fbcce181d48345e5699befa0b20b
tmsnvl95 <tmsnvl95@naver.com>
2022-07-21 09:20:20 +0900

    0720 추가 update

--- python/0719_swea_2029.py
+++ python/0719_swea_2029.py
@@ -5,7 +5,7 @@ t = int(input())
 
 for i in range(1, t + 1):
   a, b = list(map(int, input().split()))
-  print('#{} {} {}'.format(i, a // b, a % b)) #몫을 출력, 나머지를 출력 
-  #format 함수는 문자열을 출력할 때, 서식지정자를 사용하여 출력하고자 할 때
-
+  print(f'#{i} { a//b} {a % b}')
+#몫을 출력, 나머지를 출력 
+#format 함수는 문자열을 출력할 때, 서식지정자를 사용하여 출력하고자 할 때
 
---
+++ python/0720_swea_1284.py
@@ -0,0 +1,10 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+
+# a사는 1l * p원
+# b사는 
+# r이하: Q
+# r이하: q +s*()
+# q + ()
+t = int(input())
+for test_case in range(1, t+1):
