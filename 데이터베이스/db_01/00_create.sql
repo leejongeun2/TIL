a42ad8820c42b8b2c1306484320b2c76bce14db6
tmsnvl95 <tmsnvl95@naver.com>
2022-08-18 00:32:02 +0900

    0817

--- 데이터베이스/db_01/00_create.sql
+++ 데이터베이스/db_01/00_create.sql
@@ -14,9 +14,12 @@ smoking INTEGER NOT NULL,
 is_drinking BOOLEAN NOT NULL
 );
 
+--8/16(화)
 -- csv import 하기
 .mode csv 
 .import health.csv healthcare
+
+SELECT * FROM healthcare
 --1번
 SELECT COUNT(*) FROM healthcare;
 
@@ -33,3 +36,59 @@ select count(*) from healthcare where va_left >= 2.0 and va_right >= 2.0;
 select * from healthcare where va_left >= 2.0 and va_right >= 2.0;
 --7번
 select distinct sido from healthcare;
+
+
+--8/17
+--1번
+SELECT COUNT(*) FROM healthcare;
+
+--2번
+select min(age), max(age) from healthcare;
+
+--3번
+select min(height), max(weight) from healthcare;
+
+--4번
+select height from healthcare where height >= 160 and height<=170;
+
+--5번
+select * from healthcare where is_drinking = 1 and waist <> "" order by waist desc limit 5;
+
+--6번
+
+select count(*) from healthcare where va_left >= 1.5 and va_right >= 1.5 and is_drinking = 1
+
+--7번
+
+select count(*) from healthcare where blood_pressure < 120
+
+--8번
+
+select avg(waist) from healthcare where blood_pressure >= 140
+
+--9번
+
+select avg(height), avg(weight) from healthcare where gender =1
+
+--10번 
+select id, height, weight from healthcare where height=195 order by weight desc limit 1 offset 1
+
+select id, height, weight from healthcare order by height desc, weight desc limit 1 offset 1
+
+select id, height, weight from healthcare order by height desc, weight desc
+
+
+
+
+
+--11번
+select count(*) from healthcare where (weight/((height*height)*0.0001)>=30);
+
+--12번
+select id, weight/((height*height)*0.0001) AS bmi FROM healthcare where smoking = 3 order by weight/((height*height)*0.0001) desc limit 5
+
+--12번 연습
+SELECT weight, (height  * height) * 0.0001 , weight / ((height * height) * 0.0001), weight / ((height*0.01)*(height*0.01))FROM healthcare 
+        
+SELECT ((height * height) * 0.0001) FROM healthcare
+SELECT weight, (height  / 100) , (height * 0.01)  FROM healthcare        
---
+++ 데이터베이스/db_01/users.sql
@@ -0,0 +1,12 @@
+--테이블 전체 조회
+select * from users;
+
+---테이블 레코드의 갯수
+select count(*) from users;
+
+--나이가 30이상인 유저의 모든 컬럼 정보를 조회하려면? 
+select * from users where age >=30;
+
+-- users 테이블에서 age가 30 이상인 유저의 이름만 조회하려면?
+
+select * frist_name from users wh
