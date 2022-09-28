a4d68ca684e842a519f0df9e3d1fe7b490639a48
tmsnvl95 <tmsnvl95@naver.com>
2022-09-28 20:17:02 +0900

    0928_crd

--- Django/practice/0926/p_0926/urls.py
+++ Django/practice/0926/p_0926/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/', include('articles.urls')),
     path('pratices/', include('pratices.urls')),
-    
+    path('posts/', include('posts.urls')),
 ]
---
+++ Django/practice/0926/posts/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+"""
+메인(r): 게시글 목록(/posts/) / 게시글 상세
+작성(c): 글을 작 성하는 페이지(/posts/new/) / 작성완료
+수정(u): 글을 수정하는 페이지 / 수정 완료
+삭제(d): 글을 삭제 완료 
+"""
+
+app_name = 'posts'
+urlpatterns = [
+    path('', views.index, name='index'), 
+    path('new/', views.new, name='new'),
+    path('create/', views.create, name='create'),
+    path('delete/<int:pk>', views.delete, name='delete'),
+]
