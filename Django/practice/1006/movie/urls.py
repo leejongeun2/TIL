37cd36b8726a009c94eab1e72f422d36e7f70909
tmsnvl95 <tmsnvl95@naver.com>
2022-10-10 00:28:36 +0900

    1006_개인

--- Django/practice/1006/movie/urls.py
+++ Django/practice/1006/movie/urls.py
@@ -3,8 +3,13 @@ from . import views
 
 app_name = 'movie'
 
+# 기본이 get임
+
 urlpatterns = [
   # http://127.0.0.1:8000/movie/
   path('', views.index, name='index'),
-  path('create/', views.create, name='create')
+  path('create/', views.create, name='create'),
+  path('<int:pk>/', views.detail, name='detail'),
+  path('<int:pk>/update/', views.update, name='update'),
+  path('<int:pk>/delete/', views.delete, name='delete'),
 ]
--- Django/practice/1006/movie/views.py
+++ Django/practice/1006/movie/views.py
@@ -6,9 +6,9 @@ from .models import Movie
 # Create your views here.
 
 def index(request):
-    movie = Movie.objects.order_by('-pk')
+    movies = Movie.objects.order_by('-pk')
     context = {
-        'movie':movie
+        'movies':movies
     }
     return render(request, 'movie/index.html', context)
 
@@ -16,14 +16,51 @@ def create(request):
     # 실제 db에 저장하는 로직
     if request.method == 'POST':
 
-        movie_form = MovieForm(request.POST)
+        movie_form = MovieForm(request.POST) # 사용자가 작성한 내용
         if movie_form.is_valid():
             movie_form.save()
             return redirect('movie:index')
     else:
-        movie_form = MovieForm()
+        movie_form = MovieForm() # 틀만 제공
     context = {
         'movie_form': movie_form
     }
     return render(request, 'movie/new.html', context)
+
+def detail(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    context = {
+        'movie' : movie
+    }
+    return render(request, 'movie/detail.html', context)
+
+
+
+def update(request, pk):
+    movie = Movie.objects.get(pk=pk)
+
+    if request.method == 'POST':
+        movie_form = MovieForm(request.POST, instance=movie) #  movie = Movie.objects.get(pk=pk)
+        if movie_form.is_valid():
+            movie_form.save()
+            return redirect('movie:detail', movie.pk)
+    else:
+        movie_form  = MovieForm(instance=movie)
+
+    context = {
+        'movie_form' : movie_form
+        }
+    return render(request, 'movie/update.html', context)
+
+
+def delete(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    if request.method == 'POST':
+        movie.delete()
+        return redirect('movie:index')
+    context = {
+        'movie':movie
+    }
+    return render(request, 'movie/index.html', context)
+
 
