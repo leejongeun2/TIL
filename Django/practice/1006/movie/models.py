4e256889d11a3ae954dff26e4c654f9777512a90
tmsnvl95 <tmsnvl95@naver.com>
2022-10-18 16:23:49 +0900

    1017_thumbnail

--- Django/practice/1006/movie/models.py
+++ Django/practice/1006/movie/models.py
@@ -1,4 +1,4 @@
-from imagekit.models import ProcessedImageField
+from imagekit.models import ProcessedImageField, ImageSpecField
 from imagekit.processors import ResizeToFill
 from django.db import models
 
@@ -9,8 +9,11 @@ class Movie(models.Model): # Movie모델 클래스는 models에 있는 model을 
     summary = models.TextField()
     running_time = models.IntegerField()
     content = models.TextField()
-    image = ProcessedImageField(upload_to='images/', blank=True, 
-    processors=[ResizeToFill(1200, 960)],
-    format='JPEG',
-    options={'quality':80}) # 항상 업로드하는 것은 아니니 blank=true
-# 사용자가 입력한 이미지를 어디로 업로드 할 것이냐!
+    image = ProcessedImageField(upload_to='images/', blank=True, # true인 경우, 필드를 비워둘 수 있음
+                                processors=[ResizeToFill(1200, 960)],
+                                format='JPEG',
+                                options={'quality':80}) # 항상 업로드하는 것은 아니니 blank=true # 사용자가 입력한 이미지를 어디로 업로드 할 것이냐!
+    thumbnail = ProcessedImageField(upload_to='images/', blank=True,
+                                    processors=[ResizeToFill(1200,960)], 
+                                    format='JPEG',
+                                    options={'quality':80})
